088fda02e4e2a6a2f2907e15d8b0bb38755787a7
Paweł Reguła <pawel.regula92@gmail.com>
2025-01-26 16:41:15 +0100

    Added App.config

--- CLIENT/HardkorowyKodsu.Infrastructure/Extensions.cs
+++ CLIENT/HardkorowyKodsu.Infrastructure/Extensions.cs
@@ -1,4 +1,5 @@
-﻿using HardkorowyKodsu.Application.Abstractions;
+﻿using System.Configuration;
+using HardkorowyKodsu.Application.Abstractions;
 using HardkorowyKodsu.Application.DTO;
 using HardkorowyKodsu.Application.Queries;
 using HardkorowyKodsu.Infrastructure.DAL.Handlers;
@@ -10,14 +11,16 @@ namespace HardkorowyKodsu.Infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
+            string apiBaseUrl = ConfigurationManager.AppSettings["ApiBaseUrl"];
+
             services.AddHttpClient<IQueryHandler<GetTablesAndViews, IEnumerable<TableAndViewDto>>, GetTablesAndViewsHandler>((serviceProvider, client) =>
             {
-                client.BaseAddress = new Uri("https://localhost:5000");
+                client.BaseAddress = new Uri(apiBaseUrl);
             });
 
             services.AddHttpClient<IQueryHandler<GetColumns, IEnumerable<ColumnDto>>, GetColumnsHandler>((serviceProvider, client) =>
             {
-                client.BaseAddress = new Uri("https://localhost:5000");
+                client.BaseAddress = new Uri(apiBaseUrl);
             });
 
             return services;
